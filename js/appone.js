2a085ba7193bc0f0c30d9b0254d3da46570e8346
Bryan Jerome Valiente <hollandbyker@Bryans-MacBook-Pro.local>
2022-10-13 14:11:59 -0700

    first commit I hope to learn this"

---
+++ js/appone.js
@@ -0,0 +1,81 @@
+// var car = "buggati"
+/**
+ * steps for challenge
+ * 1. create a js file and in it create an array that has every persons name, age, and email(needs to have objects duh)
+ * 2.Create a for loop that 
+ */
+ const cohort = [
+    {
+     name: 'Julian',
+     age: '16',
+     gmail: 'jdawg.chapulin@gmail.com',
+ },
+ {
+     name: 'Bryan',
+     age: '16',
+     gmail: 'bryanjeromevaliente@gmail.com',
+ },
+ {
+     name: 'Angelo',
+     age: '15',
+     gmail: 'drsagaming@gmail.com',
+ },
+ {
+     name: 'Daniel',
+     age: '16',
+     gmail: 'this1rand0mguytyler@gmail.com',
+ },
+ {
+     name: 'Robert',
+     age: '15',
+     gmail: 'rvel06018@gmail.com',
+ },
+ {
+     name: 'Benji',
+     age: '15',
+     gmail: 'Ba4750323@gmail.com',
+ },
+ {
+     name: 'Drew',
+     age: '17',
+     gmail: 'imaayko@gmail.com',
+ },
+ {
+     name: 'Ralph',
+     age: '16',
+     gmail: 'rlopez90062@gmail.com',
+ },
+ {
+     name: 'Mohammed',
+     age: '16',
+     gmail: 'mohammedhossane0.0@gmail.com',
+ },
+ {
+     name: 'Henry',
+     age: '16',
+     gmail: 'henrymtz0390@gmail.com',
+ },
+ {
+     name: 'Renzo',
+     age: '17',
+     gmail: 'darkgalaxy735@gmail.com',
+ },
+ {
+     name: 'Jeffery',
+     age: '15',
+     gmail: 'hernandezjeffrey2510@gmail.com',
+ },
+ ]
+
+//  for(let i =0; i<10; i++){
+//     console.log(i);
+//  }
+const myFavCars = ["bengi", "julian", "renzo"];
+ const myFavPeople = ["bengi", "julian", "renzo"];
+// console.log(myFavCars.length);
+// loops
+for(let i = 0; i < myFavPeople.length; i++){
+    console.log(i);
+    console.log(myFavCars[i])
+}
+
